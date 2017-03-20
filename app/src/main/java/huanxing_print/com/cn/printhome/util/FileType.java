a8e78c9d47d33b85ad08670f0325dd81ff6589e5
zzzsyzxl <494852668@qq.com>
2017-03-20 09:08:19 +0800

    lgh

--- app/src/main/java/huanxing_print/com/cn/printhome/util/FileType.java
+++ app/src/main/java/huanxing_print/com/cn/printhome/util/FileType.java
@@ -105,4 +105,40 @@ public class FileType {
         }
         return isImageFile;
     }
+
+    public static final int TYPE_DOC = 1;
+    public static final int TYPE_DOCX = 2;
+    public static final int TYPE_PDF = 3;
+    public static final int TYPE_PPT = 4;
+    public static final int TYPE_PPTX = 5;
+    public static final int TYPE_IMG = 6;
+
+    public static final int getPrintType(String path) {
+        FileType fileType = new FileType();
+        fileType.initReflect();
+        int type = fileType.getMediaFileType(path);
+        if (fileType.isImageFile(type)) {
+            return TYPE_IMG;
+        }
+        String[] strs = path.split("\\.");
+        String extension = strs[strs.length - 1];
+        if ("doc".equalsIgnoreCase(extension))
+            return TYPE_DOC;
+        if ("docx".equalsIgnoreCase(extension))
+            return TYPE_DOCX;
+        if ("pdf".equalsIgnoreCase(extension))
+            return TYPE_PDF;
+        if ("ppt".equalsIgnoreCase(extension))
+            return TYPE_PPT;
+        if ("pptx".equalsIgnoreCase(extension))
+            return TYPE_PPTX;
+        return -1;
+    }
+
+    public static final boolean isPrintType(String path) {
+        if (getPrintType(path) > 0) {
+            return true;
+        }
+        return false;
+    }
 }
