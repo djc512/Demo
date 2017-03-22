bc7a88be92ffc0096b2882c1e42e1e3bc1fce56e
djc <617714267@qq.com>
2017-03-22 16:40:45 +0800

    设置界面与打印协议

---
+++ app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/my/XieYiActivity.java
@@ -0,0 +1,23 @@
+package huanxing_print.com.cn.printhome.ui.activity.my;
+
+import android.os.Bundle;
+
+import huanxing_print.com.cn.printhome.R;
+import huanxing_print.com.cn.printhome.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/3/22 0022.
+ */
+
+public class XieYiActivity extends BaseActivity {
+    @Override
+    protected BaseActivity getSelfActivity() {
+        return this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_xieyi);
+    }
+}
