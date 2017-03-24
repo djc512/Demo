a7ece4755693d3e15082fc2271ef19a6df0808f2
djc <617714267@qq.com>
2017-03-24 18:47:20 +0800

    打印详情

--- app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/my/DaYinActivity.java
+++ app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/my/DaYinActivity.java
@@ -1,6 +1,9 @@
 package huanxing_print.com.cn.printhome.ui.activity.my;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import huanxing_print.com.cn.printhome.R;
 import huanxing_print.com.cn.printhome.base.BaseActivity;
@@ -10,7 +13,10 @@ import huanxing_print.com.cn.printhome.util.CommonUtils;
  * Created by Administrator on 2017/3/17 0017.
  */
 
-public class DaYinActivity extends BaseActivity {
+public class DaYinActivity extends BaseActivity implements View.OnClickListener{
+    private LinearLayout ll_back;
+    private RecyclerView rv_dingdan;
+
     @Override
     protected BaseActivity getSelfActivity() {
         return this;
@@ -21,5 +27,20 @@ public class DaYinActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         CommonUtils.initSystemBarGreen(this);
         setContentView(R.layout.activty_user_dayin);
+        initView();
+    }
+
+    private void initView() {
+        ll_back = (LinearLayout) findViewById(R.id.ll_back);
+        rv_dingdan = (RecyclerView) findViewById(R.id.rv_dingdan);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ll_back:
+                finish();
+                break;
+        }
     }
 }
