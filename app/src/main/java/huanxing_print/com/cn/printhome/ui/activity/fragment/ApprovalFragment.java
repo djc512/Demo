88921cc9ad1d50f6d65425cf08396e751f75cf8e
yanghao <yangh@ecostar.com.cn>
2017-05-04 20:49:17 +0800

    审批界面和item布局

--- app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/fragment/ApprovalFragment.java
+++ app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/fragment/ApprovalFragment.java
@@ -1,24 +1,50 @@
 package huanxing_print.com.cn.printhome.ui.activity.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import huanxing_print.com.cn.printhome.R;
+import huanxing_print.com.cn.printhome.base.BaseFragment;
 
 /**
  * Created by Administrator on 2017/5/4 0004.
  */
 
-public class ApprovalFragment extends Fragment {
+public class ApprovalFragment extends BaseFragment {
 
-    @Nullable
+    private Context mContext;
+    ListView lv;
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView tv = new TextView(getActivity());
-        tv.setText("待审批");
-        return tv;
+    protected void init() {
+        mContext = getActivity();
+        initView();
+
+
+    }
+
+    private void initView() {
+        lv = (ListView) findViewById(R.id.lv_frag_approval);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected int getContextView() {
+        return R.layout.frag_approval;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 }
