378c224fbdace39cd1a839c115874a00981fd84d
djc <617714267@qq.com>
2017-05-04 15:21:11 +0800

    审批主界面

--- app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/approval/ApprovalActivity.java
+++ app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/approval/ApprovalActivity.java
@@ -2,37 +2,38 @@ package huanxing_print.com.cn.printhome.ui.activity.approval;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import huanxing_print.com.cn.printhome.R;
-import huanxing_print.com.cn.printhome.base.BaseActivity;
+import huanxing_print.com.cn.printhome.ui.activity.fragment.ApprovalFragment;
+import huanxing_print.com.cn.printhome.ui.activity.fragment.ApprovalNoFragment;
+import huanxing_print.com.cn.printhome.ui.adapter.ViewPagerAdapter;
 import huanxing_print.com.cn.printhome.util.CommonUtils;
 
 /**
  * Created by Administrator on 2017/5/4 0004.
  */
 
-public class ApprovalActivity extends BaseActivity {
+public class ApprovalActivity extends FragmentActivity implements View.OnClickListener {
     private ImageView iv_back;
-    private LinearLayout ll_back;
     private TextView tv_approval;
-    private LinearLayout ll_approval;
     private TextView tv_approval_no;
-    private LinearLayout ll_approval_no;
     private View view_approval;
-    private LinearLayout ll_container;
+    private ViewPager vp_approval;
     private int llWidth;
     private int tvWidth;
     private Context ctx;
-
-    @Override
-    protected BaseActivity getSelfActivity() {
-        return this;
-    }
+    private List<Fragment> fragments;
+    private ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,40 +48,85 @@ public class ApprovalActivity extends BaseActivity {
 
     private void initView() {
         iv_back = (ImageView) findViewById(R.id.iv_back);
-        ll_back = (LinearLayout) findViewById(R.id.ll_back);
         tv_approval = (TextView) findViewById(R.id.tv_approval);
-        ll_approval = (LinearLayout) findViewById(R.id.ll_approval);
         tv_approval_no = (TextView) findViewById(R.id.tv_approval_no);
-        ll_approval_no = (LinearLayout) findViewById(R.id.ll_approval_no);
         view_approval = findViewById(R.id.view_approval);
-        ll_container = (LinearLayout) findViewById(R.id.ll_container);
-    }
-
-
-    ViewTreeObserver.OnGlobalLayoutListener observer = new ViewTreeObserver.OnGlobalLayoutListener() {
-        @Override
-        public void onGlobalLayout() {
-            llWidth = ll_approval.getWidth();
-            tvWidth = tv_approval.getWidth();
-            setViewWidth(tvWidth);
-        }
-    };
-
-    private void setViewWidth(int width) {
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(width, CommonUtils.dip2px(ctx, 3));
-        params.leftMargin = (llWidth - width) / 2;
-        view_approval.setLayoutParams(params);
+        vp_approval = (ViewPager) findViewById(R.id.vp_approval);
     }
 
     private void initData() {
+
+        fragments = new ArrayList<>();
+        ApprovalFragment approvalFragment = new ApprovalFragment();
+        ApprovalNoFragment approvalnoFragment = new ApprovalNoFragment();
+        fragments.add(approvalFragment);
+        fragments.add(approvalnoFragment);
+
+        adapter = new ViewPagerAdapter(getSupportFragmentManager(), fragments);
+        vp_approval.setAdapter(adapter);
     }
 
     private void initListener() {
+        iv_back.setOnClickListener(this);
+        tv_approval.setOnClickListener(this);
+        tv_approval_no.setOnClickListener(this);
+
+        vp_approval.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(tvWidth, CommonUtils.dip2px(ctx, 3));
+                if (lp != null && positionOffset != 0) {
+                    lp.leftMargin = (int) ((position + positionOffset) * llWidth) + (llWidth - tvWidth) / 2;
+                    view_approval.setLayoutParams(lp);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0) {
+                    tv_approval.setTextColor(getResources().getColor(R.color.blue));
+                    tv_approval_no.setTextColor(getResources().getColor(R.color.black2));
+                } else {
+                    tv_approval_no.setTextColor(getResources().getColor(R.color.blue));
+                    tv_approval.setTextColor(getResources().getColor(R.color.black2));
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        tv_approval.getViewTreeObserver().addOnGlobalLayoutListener(observer);
+        int w = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        int h = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        tv_approval.measure(w, h);
+        tvWidth = tv_approval.getMeasuredWidth();
+
+        int screenWidth = getWindowManager().getDefaultDisplay().getWidth();
+        llWidth = screenWidth / 2;
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(tvWidth, CommonUtils.dip2px(ctx, 2));
+        lp.leftMargin = (llWidth - tvWidth) / 2;
+        view_approval.setLayoutParams(lp);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.tv_approval:
+                vp_approval.setCurrentItem(0);
+                break;
+            case R.id.tv_approval_no:
+                vp_approval.setCurrentItem(1);
+                break;
+        }
     }
 }
