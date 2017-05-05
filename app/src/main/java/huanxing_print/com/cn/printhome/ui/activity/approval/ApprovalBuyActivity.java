ef52fe4a7ce76670d830982c479dca3cf8f72385
yanghao <yangh@ecostar.com.cn>
2017-05-05 16:53:30 +0800

    采购审批逻辑,点击界面收缩

--- app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/approval/ApprovalBuyActivity.java
+++ app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/approval/ApprovalBuyActivity.java
@@ -6,10 +6,12 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import huanxing_print.com.cn.printhome.R;
-import huanxing_print.com.cn.printhome.base.BaseActivity;
-import huanxing_print.com.cn.printhome.util.CommonUtils;
+
 
 /**
  * Created by Administrator on 2017/5/5 0005.
@@ -17,6 +19,15 @@ import huanxing_print.com.cn.printhome.util.CommonUtils;
 public class ApprovalBuyActivity extends Activity implements View.OnClickListener{
 
     public Context context;
+
+    ImageView iv_camera;
+    ImageView iv_off;
+    ImageView iv_time;
+    ImageView iv_back;
+    Button bt_commit;
+    LinearLayout ll_id;
+
+    boolean isRequestMoney =false;
 
 
     @Override
@@ -30,18 +41,67 @@ public class ApprovalBuyActivity extends Activity implements View.OnClickListene
     }
 
     private void initListener() {
+        iv_back.setOnClickListener(this);
+        iv_camera.setOnClickListener(this);
+        iv_off.setOnClickListener(this);
+        iv_time.setOnClickListener(this);
+        bt_commit.setOnClickListener(this);
+        ll_id.setOnClickListener(this);
     }
 
     private void initData() {
     }
 
     private void initView() {
+        iv_back = (ImageView) findViewById(R.id.iv_back);
+        iv_camera = (ImageView) findViewById(R.id.iv_camera);
+        iv_off = (ImageView) findViewById(R.id.iv_off);
+        iv_time = (ImageView) findViewById(R.id.iv_time);
+        bt_commit = (Button) findViewById(R.id.bt_commit);
+        ll_id = (LinearLayout) findViewById(R.id.ll_id);
 
     }
 
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_back:
+                break;
+            case R.id.iv_camera:
+                break;
+            case R.id.iv_off:
+                if (isRequestMoney){
+                    bankInformationOpen();
+                    isRequestMoney = false;
+                } else{
+                    bankInformationClose();
+                    isRequestMoney =true;
+                }
+
+                break;
+            case R.id.iv_time:
+                break;
+            case R.id.bt_commit:
+                break;
+            case R.id.ll_id:
+
+                break;
+            default:
+                break;
+        }
 
     }
+
+
+
+
+        public void bankInformationOpen(){
+            ll_id.setVisibility(View.VISIBLE);
+        }
+        public void bankInformationClose(){
+            ll_id.setVisibility(View.GONE);
+        }
+
+
 }
