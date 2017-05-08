b0801b3c8a4d9f93839bd1eab6ac21cb2c78e074
wulianshuai <tuniu123>
2017-05-08 16:29:58 +0800

    新增审批接口构建以及页面调整

---
+++ app/src/main/java/huanxing_print/com/cn/printhome/net/callback/approval/AddApprovalCallBack.java
@@ -0,0 +1,11 @@
+package huanxing_print.com.cn.printhome.net.callback.approval;
+
+import huanxing_print.com.cn.printhome.net.callback.BaseCallback;
+
+/**
+ * Created by dd on 2017/5/8.
+ */
+
+public interface AddApprovalCallBack extends BaseCallback {
+    void success(String data);
+}
