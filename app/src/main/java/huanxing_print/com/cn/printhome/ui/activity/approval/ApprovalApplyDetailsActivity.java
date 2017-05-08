dbc93328936b2532f9ff9484f078620ef0f3745e
huangtaojun <m18395555231@163.com>
2017-05-08 13:00:31 +0800

    代码提交

---
+++ app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/approval/ApprovalApplyDetailsActivity.java
@@ -0,0 +1,8 @@
+package huanxing_print.com.cn.printhome.ui.activity.approval;
+
+/**
+ * Created by htj on 2017/5/7.
+ */
+
+public class ApprovalApplyDetailsActivity {
+}
---
+++ app/src/main/java/huanxing_print/com/cn/printhome/ui/adapter/ApprovalListAdapter.java
@@ -0,0 +1,78 @@
+package huanxing_print.com.cn.printhome.ui.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import huanxing_print.com.cn.printhome.R;
+import huanxing_print.com.cn.printhome.util.approval.Info;
+
+/**
+ * Created by Administrator on 2017/3/23 0023.
+ */
+
+public class ApprovalListAdapter extends BaseAdapter {
+    private Context ctx;
+    private List<Info> list;
+
+    public ApprovalListAdapter(Context ctx, List<Info> list) {
+        this.ctx = ctx;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return 10;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        MyViewHolder holder = null;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(ctx).inflate(R.layout.item_approval,null);
+            holder = new MyViewHolder();
+            holder.tv_name = (TextView) convertView.findViewById(R.id.tv_name);
+            holder.tv_time = (TextView) convertView.findViewById(R.id.tv_time);
+            holder.tv_use = (TextView) convertView.findViewById(R.id.tv_use);
+            holder.tv_detail = (TextView) convertView.findViewById(R.id.tv_detail);
+            holder.tv_state = (TextView) convertView.findViewById(R.id.tv_state);
+
+            convertView.setTag(holder);
+        }else {
+           holder = (MyViewHolder) convertView.getTag();
+        }
+        //Info listBean = list.get(position);
+        //String date = listBean.getDate();
+        //holder.tv_time.setText(date);
+        /*holder.tv_name.setText(listBean.getName());
+        holder.tv_name.setText(listBean.getTime());
+        holder.tv_name.setText(listBean.getUse());
+        holder.tv_name.setText(listBean.getDetail());
+        holder.tv_name.setText(listBean.getId());*/
+
+        return convertView;
+    }
+    public class MyViewHolder{
+        TextView tv_name;
+        TextView tv_time;
+        TextView tv_use;
+        TextView tv_detail;
+        TextView tv_state;
+    }
+}
