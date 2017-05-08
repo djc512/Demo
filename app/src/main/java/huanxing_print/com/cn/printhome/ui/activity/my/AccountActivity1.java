3897d350494f8621efd0294825e9cfb910296db8
djc <617714267@qq.com>
2017-05-08 10:49:46 +0800

    充值页面

---
+++ app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/my/AccountActivity1.java
@@ -0,0 +1,176 @@
+package huanxing_print.com.cn.printhome.ui.activity.my;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.simple.eventbus.Subscriber;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import huanxing_print.com.cn.printhome.R;
+import huanxing_print.com.cn.printhome.base.BaseActivity;
+import huanxing_print.com.cn.printhome.model.my.ChongZhiBean;
+import huanxing_print.com.cn.printhome.net.callback.my.ChongzhiCallBack;
+import huanxing_print.com.cn.printhome.net.callback.my.OrderIdCallBack;
+import huanxing_print.com.cn.printhome.net.request.my.ChongzhiRequest;
+import huanxing_print.com.cn.printhome.net.request.my.OrderIdRequest;
+import huanxing_print.com.cn.printhome.ui.adapter.AccountCZAdapter1;
+import huanxing_print.com.cn.printhome.util.CommonUtils;
+import huanxing_print.com.cn.printhome.util.ObjectUtils;
+import huanxing_print.com.cn.printhome.util.ToastUtil;
+
+/**
+ * Created by Administrator on 2017/3/17 0017.
+ */
+
+public class AccountActivity1 extends BaseActivity implements View.OnClickListener {
+
+    private TextView tv_money;
+    private Button btn_chongzhi;
+    private LinearLayout ll_back;
+    private TextView tv_account_record;
+    private RecyclerView rv_account;
+    private List<ChongZhiBean> listBean = new ArrayList<>();
+
+    private String rechargeAmout;
+    private ChongZhiBean chongZhiBean;
+    private String totleBalance;
+    private AccountCZAdapter1 adapter1;
+
+    @Override
+    protected BaseActivity getSelfActivity() {
+        return this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CommonUtils.initSystemBar(this);
+        setContentView(R.layout.activity_user_account);
+        initView();
+        initData();
+        setListener();
+    }
+    private String amount;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!ObjectUtils.isNull(amount)) {
+            int accountMoney = Integer.parseInt(totleBalance) + Integer.parseInt(amount);
+            tv_money.setText(accountMoney+"");
+        }
+    }
+    @Subscriber(tag = "rechargeAmout")
+    private void getRechargeAmout(String rechargeAmout){
+        amount = rechargeAmout;
+    }
+
+    private void initView() {
+        tv_money = (TextView) findViewById(R.id.tv_money);
+        btn_chongzhi = (Button) findViewById(R.id.btn_chongzhi);
+
+        ll_back = (LinearLayout) findViewById(R.id.ll_back);
+        tv_account_record = (TextView) findViewById(R.id.tv_account_record);
+        rv_account = (RecyclerView) findViewById(R.id.rv_account);
+
+    }
+
+    private void initData() {
+        totleBalance = getIntent().getStringExtra("totleBalance");
+        tv_money.setText(totleBalance);
+        //通过接口获取充值数据
+        ChongzhiRequest.getChongZhi(getSelfActivity(), new MyChongzhiCallBack());
+    }
+
+    private void setListener() {
+        tv_account_record.setOnClickListener(this);
+        ll_back.setOnClickListener(this);
+        btn_chongzhi.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_chongzhi:
+                if (ObjectUtils.isNull(rechargeAmout)) {//如果没有选择充值数
+                    ToastUtil.doToast(getSelfActivity(), "请先选择充值金额");
+                    return;
+                }
+                getOrderId();
+                break;
+            case R.id.tv_account_record://充值记录
+                startActivity(new Intent(getSelfActivity(), AccountRecordActivity.class));
+                break;
+            case R.id.ll_back://返回
+                finishCurrentActivity();
+                break;
+        }
+    }
+
+    /**
+     * 获取订单号
+     */
+    private void getOrderId() {
+        OrderIdRequest.getOrderId(getSelfActivity(), rechargeAmout, new OrderIdCallBack() {
+            @Override
+            public void success(String msg, String data) {
+                toast("请求成功");
+                Intent intent = new Intent(getSelfActivity(), PayActivity.class);
+                intent.putExtra("orderId", data);
+                intent.putExtra("rechargeAmout", rechargeAmout);
+                startActivity(intent);
+            }
+
+            @Override
+            public void fail(String msg) {
+
+            }
+
+            @Override
+            public void connectFail() {
+
+            }
+        });
+
+    }
+
+    public class MyChongzhiCallBack extends ChongzhiCallBack {
+
+        @Override
+        public void success(String msg, List<ChongZhiBean> list) {
+            listBean = list;
+            toast("获取成功");
+            rv_account.setLayoutManager(new LinearLayoutManager(getSelfActivity()));
+            adapter1 = new AccountCZAdapter1(getSelfActivity(),null);
+            rv_account.setAdapter(adapter1);
+            adapter1.setOnItemClickLitener(new AccountCZAdapter1.OnItemClickLitener() {
+                @Override
+                public void onItemClick(View view, int position) {
+                    adapter1.setSeclection(position);
+                    adapter1.notifyDataSetChanged();
+                    chongZhiBean = listBean.get(position);
+                    //充值金额
+                    rechargeAmout = chongZhiBean.getRechargeAmout();
+                }
+            });
+        }
+
+        @Override
+        public void fail(String msg) {
+
+        }
+
+        @Override
+        public void connectFail() {
+
+        }
+    }
+}
---
+++ app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/my/AccountRecordActivity1.java
@@ -0,0 +1,142 @@
+package huanxing_print.com.cn.printhome.ui.activity.my;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import com.andview.refreshview.XRefreshView;
+
+import java.util.List;
+
+import huanxing_print.com.cn.printhome.R;
+import huanxing_print.com.cn.printhome.base.BaseActivity;
+import huanxing_print.com.cn.printhome.model.my.ChongZhiRecordBean;
+import huanxing_print.com.cn.printhome.net.callback.my.ChongZhiRecordCallBack;
+import huanxing_print.com.cn.printhome.net.request.my.ChongZhiRecordRequest;
+import huanxing_print.com.cn.printhome.ui.adapter.AccountRecordAdapter;
+import huanxing_print.com.cn.printhome.util.CommonUtils;
+import huanxing_print.com.cn.printhome.util.ObjectUtils;
+import huanxing_print.com.cn.printhome.util.ToastUtil;
+import huanxing_print.com.cn.printhome.view.refresh.CustomerFooter;
+
+/**
+ * Created by Administrator on 2017/3/23 0023.
+ */
+
+public class AccountRecordActivity1 extends BaseActivity implements View.OnClickListener {
+
+    private LinearLayout ll_back;
+    private ListView lv_account_record;
+    private int pageNum = 1;
+    private AccountRecordAdapter adapter;
+    private XRefreshView xrf_czrecord;
+    private boolean isLoadMore = false;
+    private List<ChongZhiRecordBean.ListBean> datalist;
+
+    @Override
+    protected BaseActivity getSelfActivity() {
+        return this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        CommonUtils.initSystemBar(this);
+        setContentView(R.layout.activity_account_record1);
+        initView();
+        initData();
+        setListener();
+    }
+
+    private void initData() {
+        //获取充值记录
+        ChongZhiRecordRequest.getCzRecord(getSelfActivity(), pageNum, new MyChongzhiRecordCallBack());
+    }
+
+    private void initView() {
+        xrf_czrecord = (XRefreshView) findViewById(R.id.xrf_czrecord);
+        ll_back = (LinearLayout) findViewById(R.id.ll_back);
+        lv_account_record = (ListView) findViewById(R.id.lv_account_record);
+    }
+
+    private void setListener() {
+        ll_back.setOnClickListener(this);
+
+        xrf_czrecord.setXRefreshViewListener(new XRefreshView.SimpleXRefreshListener() {
+
+            @Override
+            public void onRefresh() {
+                super.onRefresh();
+                isLoadMore = false;
+                datalist.clear();
+                pageNum = 1;
+                //获取充值记录
+                ChongZhiRecordRequest.getCzRecord(getSelfActivity(), pageNum, new MyChongzhiRecordCallBack());
+                xrf_czrecord.stopRefresh();
+            }
+
+            @Override
+            public void onLoadMore(boolean isSilence) {
+                super.onLoadMore(isSilence);
+                isLoadMore = true;
+                pageNum++;
+                ChongZhiRecordRequest.getCzRecord(getSelfActivity(), pageNum, new MyChongzhiRecordCallBack());
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_back:
+                finishCurrentActivity();
+                break;
+        }
+    }
+
+    private class MyChongzhiRecordCallBack extends ChongZhiRecordCallBack {
+
+        @Override
+        public void success(String msg, ChongZhiRecordBean bean) {
+            if (isLoadMore) {//如果是加载更多
+                if (!ObjectUtils.isNull(bean)) {
+                    xrf_czrecord.stopLoadMore();
+                    if (!ObjectUtils.isNull(bean.getList())) {
+                        datalist.addAll(bean.getList());
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        ToastUtil.doToast(getSelfActivity(), "没有更多数据");
+                        return;
+                    }
+                } else {
+                    ToastUtil.doToast(getSelfActivity(), "没有更多数据");
+                    xrf_czrecord.stopLoadMore();
+                    return;
+                }
+            } else {
+                datalist = bean.getList();
+                adapter = new AccountRecordAdapter(getSelfActivity(), datalist);
+                lv_account_record.setAdapter(adapter);
+            }
+            xrf_czrecord.setPullLoadEnable(true);
+            xrf_czrecord.setAutoLoadMore(false);
+            //设置在上拉加载被禁用的情况下，是否允许界面被上拉
+            xrf_czrecord.setPinnedTime(1000);
+            xrf_czrecord.setMoveForHorizontal(true);
+            xrf_czrecord.setCustomFooterView(new CustomerFooter(getSelfActivity()));
+            //设置当非RecyclerView上拉加载完成以后的回弹时间
+//            xrf_czrecord.setScrollBackDuration(300);
+        }
+
+        @Override
+        public void fail(String msg) {
+
+        }
+
+        @Override
+        public void connectFail() {
+
+        }
+    }
+}
