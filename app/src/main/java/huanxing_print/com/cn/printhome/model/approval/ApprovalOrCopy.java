854f11b3a6d064c3f2ea0ef80e8e80896bf603bf
wanghao <wang1065803059@163.com>
2017-05-13 12:05:18 +0800

    审批详情页面数据展示

--- app/src/main/java/huanxing_print/com/cn/printhome/model/approval/ApprovalOrCopy.java
+++ app/src/main/java/huanxing_print/com/cn/printhome/model/approval/ApprovalOrCopy.java
@@ -10,6 +10,9 @@ public class ApprovalOrCopy {
     private String jobNumber;//会员id
     private String name;//名字
     private long priority;//排序
+    private String status;//-1-（默认）未开始、0-审批中、2-已同意/3-已拒绝、4-申请人撤销、5-完成-未读、6-完成-
+    private String type;//0-申请人；1-审批人；2-抄送人
+    private String updateTime;//更新时间
 
     public String getFaceUrl() {
         return faceUrl;
@@ -41,5 +44,29 @@ public class ApprovalOrCopy {
 
     public void setPriority(long priority) {
         this.priority = priority;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
     }
 }
---
+++ app/src/main/java/huanxing_print/com/cn/printhome/ui/adapter/ApprovalCopyMembersAdapter.java
@@ -0,0 +1,86 @@
+package huanxing_print.com.cn.printhome.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import huanxing_print.com.cn.printhome.R;
+import huanxing_print.com.cn.printhome.model.approval.ApprovalOrCopy;
+
+/**
+ * Created by wanghao on 2017/5/13.
+ */
+
+public class ApprovalCopyMembersAdapter extends BaseAdapter{
+    private Context mContext;
+    private ArrayList<ApprovalOrCopy> mCopys = new ArrayList<ApprovalOrCopy>();
+    private LayoutInflater layoutInflater;
+    public ApprovalCopyMembersAdapter(Context context, ArrayList<ApprovalOrCopy> copys) {
+        this.mContext = context;
+        this.mCopys = copys;
+        layoutInflater = LayoutInflater.from(mContext);
+    }
+
+    public void modifyData(ArrayList<ApprovalOrCopy> copys) {
+        this.mCopys = copys;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return mCopys.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return mCopys.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+        if(null == view) {
+            holder = new ViewHolder();
+            view = layoutInflater.inflate(R.layout.item_approval_copy_member, null);
+            holder.iv_member_icon = (CircleImageView) view.findViewById(R.id.iv_copy_icon);
+            holder.tv_memberName = (TextView) view.findViewById(R.id.tv_copy_name);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+        ApprovalOrCopy copy = mCopys.get(i);
+        holder.tv_memberName.setText(copy.getName());
+        loadPic(holder.iv_member_icon, copy.getFaceUrl());
+        return view;
+    }
+    private void loadPic(final ImageView iv_user_head, String picPath) {
+        Glide.with(mContext).load(picPath).placeholder(R.drawable.iv_head).into(new SimpleTarget<GlideDrawable>() {
+            @Override
+            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                iv_user_head.setImageDrawable(resource);
+            }
+        });
+    }
+
+    class ViewHolder{
+        CircleImageView iv_member_icon;
+        TextView tv_memberName;
+    }
+}
