f73c7aa944a1e3cafa07e92c088d679fcea9061b
djc <617714267@qq.com>
2017-05-13 16:54:55 +0800

    开发票接口

--- app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/copy/HuKouClipActivity.java
+++ app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/copy/HuKouClipActivity.java
@@ -21,7 +21,6 @@ import huanxing_print.com.cn.printhome.ui.activity.print.PickPrinterActivity;
 import huanxing_print.com.cn.printhome.util.CommonUtils;
 import huanxing_print.com.cn.printhome.util.copy.PicSaveUtil;
 
-import static huanxing_print.com.cn.printhome.R.id.btn_confirm;
 import static huanxing_print.com.cn.printhome.util.copy.ClipPicUtil.ctx;
 
 /**
@@ -65,7 +64,7 @@ public class HuKouClipActivity extends BaseActivity implements View.OnClickListe
     private void initView() {
         iv_preview = (ImageView) findViewById(R.id.iv_preview);
         btn_reset = (TextView) findViewById(R.id.btn_reset);
-        btn_preview = (TextView) findViewById(btn_confirm);
+        btn_preview = (TextView) findViewById(R.id.btn_confirm);
     }
 
     private void initData() {
