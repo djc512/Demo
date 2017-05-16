1f301dcb8efcf8595542093149c54b72ec171115
wanghao <wang1065803059@163.com>
2017-05-16 10:23:53 +0800

    添加好友验证，验证内容长度限制

--- app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/contact/AddVerificationActivity.java
+++ app/src/main/java/huanxing_print/com/cn/printhome/ui/activity/contact/AddVerificationActivity.java
@@ -1,11 +1,15 @@
 package huanxing_print.com.cn.printhome.ui.activity.contact;
 
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.EditText;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import huanxing_print.com.cn.printhome.R;
 import huanxing_print.com.cn.printhome.base.BaseActivity;
@@ -26,6 +30,7 @@ import huanxing_print.com.cn.printhome.view.dialog.DialogUtils;
 public class AddVerificationActivity extends BaseActivity implements View.OnClickListener{
     private EditText et_hint_content;
     private FriendSearchInfo friendSearchInfo;
+    private InputLengthFilter filter;
     @Override
     protected BaseActivity getSelfActivity() {
         return this;
@@ -43,6 +48,8 @@ public class AddVerificationActivity extends BaseActivity implements View.OnClic
 
     private void initView() {
         et_hint_content = (EditText) findViewById(R.id.et_hint_content);
+        filter = new InputLengthFilter(30);
+        et_hint_content.setFilters(new InputFilter[]{filter});
     }
 
     private void setListener() {
@@ -95,4 +102,60 @@ public class AddVerificationActivity extends BaseActivity implements View.OnClic
             toastConnectFail();
         }
     };
+
+    private class InputLengthFilter implements InputFilter {
+        int MAX_EN;
+        String regEx = "[\\u4e00-\\u9fa5]";
+
+        public InputLengthFilter(int mAX_EN) {
+            super();
+            MAX_EN = mAX_EN;
+        }
+
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end,
+                                   Spanned dest, int dstart, int dend) {
+            int destCount = dest.toString().length()
+                    + getChineseCount(dest.toString());
+            int sourceCount = source.toString().length()
+                    + getChineseCount(source.toString());
+            if (destCount + sourceCount > MAX_EN) {
+                int surplusCount = MAX_EN - destCount;
+                String result = "";
+                int index = 0;
+                while (surplusCount > 0) {
+                    char c = source.charAt(index);
+                    if (isChinest(c + "")) {
+                        if (sourceCount >= 2) {
+                            result += c;
+                        }
+                        surplusCount = surplusCount - 2;
+                    } else {
+                        result += c;
+                        surplusCount = surplusCount - 1;
+                    }
+                    index++;
+                }
+                return result;
+            } else {
+                return source;
+            }
+        }
+
+        private int getChineseCount(String str) {
+            int count = 0;
+            Pattern p = Pattern.compile(regEx);
+            Matcher m = p.matcher(str);
+            while (m.find()) {
+                for (int i = 0; i <= m.groupCount(); i++) {
+                    count = count + 1;
+                }
+            }
+            return count;
+        }
+
+        private boolean isChinest(String source) {
+            return Pattern.matches(regEx, source);
+        }
+    }
 }
