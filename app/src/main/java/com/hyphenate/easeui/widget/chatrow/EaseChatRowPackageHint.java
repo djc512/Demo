1ed73b2ec07baee94928715135e0ea17e8740b5f
huangtaojun <m18395555231@163.com>
2017-05-23 15:00:52 +0800

    红包bug

--- app/src/main/java/com/hyphenate/easeui/widget/chatrow/EaseChatRowPackageHint.java
+++ app/src/main/java/com/hyphenate/easeui/widget/chatrow/EaseChatRowPackageHint.java
@@ -2,6 +2,9 @@ package com.hyphenate.easeui.widget.chatrow;
 
 import android.content.Context;
 import android.content.Intent;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
@@ -42,12 +45,11 @@ public class EaseChatRowPackageHint extends EaseChatRowText {
         // 设置内容
         String msg = message.getStringAttribute(EaseConstant.MESSAGE_HINT_MESSAGE, "");
         if (!ObjectUtils.isNull(msg)) {
-            txt_message.setText(msg.substring(0, msg.length() - 2));
+            SpannableStringBuilder style=new SpannableStringBuilder(msg);
+            style.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.yellow3)),msg.length() - 2,msg.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            // 设置内容
+            txt_message.setText(style);
         }
-//        EMTextMessageBody txtBody = (EMTextMessageBody) message.getBody();
-//        Spannable span = EaseSmileUtils.getSmiledText(context, txtBody.getMessage());
-//        // 设置内容
-//        txt_message.setText(span, TextView.BufferType.SPANNABLE);
         handleTextMessage();
     }
 
