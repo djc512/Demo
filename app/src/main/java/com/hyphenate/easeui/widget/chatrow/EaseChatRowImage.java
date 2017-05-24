951dfd22fd5d7bfffe931dbb7349cacec1883099
wulianshuai <tuniu123>
2017-05-24 20:45:45 +0800

    转发头像和ios消息头像问题

--- app/src/main/java/com/hyphenate/easeui/widget/chatrow/EaseChatRowImage.java
+++ app/src/main/java/com/hyphenate/easeui/widget/chatrow/EaseChatRowImage.java
@@ -18,7 +18,6 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
-import com.hyphenate.chat.EMCmdMessageBody;
 import com.hyphenate.chat.EMFileMessageBody;
 import com.hyphenate.chat.EMImageMessageBody;
 import com.hyphenate.chat.EMMessage;
@@ -32,6 +31,8 @@ import org.greenrobot.eventbus.EventBus;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import huanxing_print.com.cn.printhome.R;
 import huanxing_print.com.cn.printhome.model.chat.RefreshEvent;
@@ -39,8 +40,8 @@ import huanxing_print.com.cn.printhome.ui.activity.chat.CreateGroupChatActivity;
 import huanxing_print.com.cn.printhome.util.CircleTransform;
 import huanxing_print.com.cn.printhome.util.ObjectUtils;
 import huanxing_print.com.cn.printhome.util.PreViewUtil;
-import huanxing_print.com.cn.printhome.util.ToastUtil;
 import huanxing_print.com.cn.printhome.util.ToastUtils;
+import huanxing_print.com.cn.printhome.view.dialog.DialogUtils;
 import huanxing_print.com.cn.printhome.view.popupwindow.PopupList;
 
 public class EaseChatRowImage extends EaseChatRowFile {
@@ -53,6 +54,7 @@ public class EaseChatRowImage extends EaseChatRowFile {
     private LinearLayout lin_group;
     private ArrayList<String> popupMenuItemList;
     private String localFilePath;
+    private ExecutorService service = Executors.newSingleThreadExecutor();
 
     public EaseChatRowImage(Context context, EMMessage message, int position, BaseAdapter adapter) {
         super(context, message, position, adapter);
@@ -77,6 +79,13 @@ public class EaseChatRowImage extends EaseChatRowFile {
 
     @Override
     protected void onSetUpView() {
+
+        popupMenuItemList = new ArrayList<>();
+        popupMenuItemList.add("打印");
+        popupMenuItemList.add("转发");
+        popupMenuItemList.add("保存");
+        popupMenuItemList.add("删除");
+
         EMImageMessageBody body = (EMImageMessageBody) message.getBody();
         localFilePath = body.getLocalUrl();
         Log.d("CMCC", "localFilePath:" + localFilePath);
@@ -170,13 +179,8 @@ public class EaseChatRowImage extends EaseChatRowFile {
 
     @Override
     protected void onBubbleClick() {
-        File file = new File(localFilePath);
-        if (!ObjectUtils.isNull(file) && file.exists()) {
-            //预览
-            PreViewUtil.preview(context, localFilePath);
-        } else {
-            downloadImage(message.getMsgId());
-        }
+        downloadImage(message.getMsgId());
+
 //        Intent intent = new Intent(context, EaseShowBigImageActivity.class);
 //        File file = new File(imgBody.getLocalUrl());
 //        if (file.exists()) {
@@ -265,21 +269,9 @@ public class EaseChatRowImage extends EaseChatRowFile {
 
     @Override
     protected void onBubbleLongClick() {
+
         Log.d("CMCC", "onBubbleLongClick触发了");
 
-        File file = new File(localFilePath);
-        if (!file.exists()) {
-            //不存在就去下载
-            ToastUtil.doToast(context, "正在下载...");
-            downloadlongImage(message.getMsgId());
-            return;
-        }
-
-        popupMenuItemList = new ArrayList<>();
-        popupMenuItemList.add("打印");
-        popupMenuItemList.add("转发");
-        popupMenuItemList.add("保存");
-        popupMenuItemList.add("删除");
         PopupList popupList = new PopupList(context);
         popupList.bind(bubble, popupMenuItemList, new PopupList.PopupListListener() {
             @Override
@@ -292,84 +284,29 @@ public class EaseChatRowImage extends EaseChatRowFile {
                 switch (position) {
                     case 0:
                         //打印
-//                        Log.d("CMCC", "打印");
-                        PreViewUtil.preview(context, localFilePath);
+                        downloadlongImage(message.getMsgId(), 0);
                         break;
                     case 1:
                         //转发
-//                        Log.d("CMCC", "转发");
-//                        Log.d("CMCC", "localFilePath:" + localFilePath);
-                        //跳转到选择联系人界面只能单选
-                        Intent intent = new Intent(context, CreateGroupChatActivity.class);
-                        intent.putExtra("imgUrl", localFilePath);
-                        context.startActivity(intent);
+                        downloadlongImage(message.getMsgId(), 1);
                         break;
                     case 2:
-                        EMImageMessageBody body = (EMImageMessageBody) message.getBody();
                         //保存
-                        Log.d("CMCC", "保存");
-                        // 其次把文件插入到系统图库
-                        try {
-                            String url = MediaStore.Images.Media.insertImage(context.getContentResolver(),
-                                    localFilePath, body.getFileName(), null);
-                            Log.d("CMCC", "url:" + url);
-                            // 最后通知图库更新
-                            context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse(url)));
-                            ToastUtils.showToast(context, "保存成功了...");
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
-                        }
-
+                        downloadlongImage(message.getMsgId(), 2);
                         break;
                     case 3:
-                        //删除记录
-                        Log.d("CMCC", "删除");
-                        if (message.direct() == EMMessage.Direct.SEND) {
-                            //只能删除本人发出的消息
-                            //发送透传消息代码如下：
-                            String action = context.getString(R.string.REMOVE);
-                            EMMessage cmdMessage = EMMessage.createSendMessage(EMMessage.Type.CMD);
-
-                            EMCmdMessageBody cmdBody = new EMCmdMessageBody(action);
-                            Log.d("CMCC", "111111");
-                            if (message.getChatType() == ChatType.GroupChat ||
-                                    message.getChatType() == ChatType.ChatRoom) {
-                                cmdMessage.setChatType(ChatType.GroupChat);
-                                String toChatUserName = message.getTo();
-                                Log.d("CMCC", "toChatUserName------>" + toChatUserName);
-                                cmdMessage.setTo(toChatUserName);
-                                //删除掉本地消息
-                                EMClient.getInstance().chatManager()
-                                        .getConversation(toChatUserName).removeMessage(message.getMsgId());
-                            } else {
-                                String toChatUserName = message.getFrom();
-                                Log.d("CMCC", "toChatUserName------>" + toChatUserName);
-                                cmdMessage.setFrom(toChatUserName);
-                                //删除掉本地消息
-                                EMClient.getInstance().chatManager()
-                                        .getConversation(toChatUserName).removeMessage(message.getMsgId());
-                            }
-
-                            String msgId = message.getMsgId();
-                            Log.d("CMCC", "msgIdsend-------->" + msgId);
-                            cmdMessage.setAttribute("msgid", msgId);
-                            cmdMessage.addBody(cmdBody);
-                            EMClient.getInstance().chatManager().sendMessage(cmdMessage);
-                            Log.d("CMCC", "发送透传success");
+                        //删除本地
+                        if (message.getChatType() == ChatType.GroupChat ||
+                                message.getChatType() == ChatType.ChatRoom) {
+                            String toChatUserName = message.getTo();
+                            //删除掉本地消息
+                            EMClient.getInstance().chatManager()
+                                    .getConversation(toChatUserName).removeMessage(message.getMsgId());
                         } else {
-                            //删除本地
-                            if (message.getChatType() == ChatType.GroupChat ||
-                                    message.getChatType() == ChatType.ChatRoom) {
-                                String toChatUserName = message.getTo();
-                                //删除掉本地消息
-                                EMClient.getInstance().chatManager()
-                                        .getConversation(toChatUserName).removeMessage(message.getMsgId());
-                            } else {
-                                String toChatUserName = message.getFrom();
-                                //删除掉本地消息
-                                EMClient.getInstance().chatManager()
-                                        .getConversation(toChatUserName).removeMessage(message.getMsgId());
-                            }
+                            String toChatUserName = message.getFrom();
+                            //删除掉本地消息
+                            EMClient.getInstance().chatManager()
+                                    .getConversation(toChatUserName).removeMessage(message.getMsgId());
                         }
                         //发消息刷新
                         RefreshEvent event = new RefreshEvent();
@@ -390,14 +327,16 @@ public class EaseChatRowImage extends EaseChatRowFile {
         File temp = new File(localFilePath);
         if (temp != null && temp.exists()) {
             //存在就不要下载
-            Log.d("CMCC", "存在就不要下载");
+            PreViewUtil.preview(context, localFilePath);
             return;
         }
+        DialogUtils.showProgressDialog(context, "下载中");
         final String tempPath = temp.getParent() + "/temp_" + temp.getName();
         final EMCallBack callback = new EMCallBack() {
             public void onSuccess() {
                 Log.d("CMCC", "onSuccess");
                 //预览
+                DialogUtils.closeProgressDialog();
                 PreViewUtil.preview(context, localFilePath);
             }
 
@@ -407,6 +346,7 @@ public class EaseChatRowImage extends EaseChatRowFile {
                 if (file.exists() && file.isFile()) {
                     file.delete();
                 }
+                DialogUtils.closeProgressDialog();
             }
 
             public void onProgress(final int progress, String status) {
@@ -425,20 +365,79 @@ public class EaseChatRowImage extends EaseChatRowFile {
      * download image
      */
     @SuppressLint("NewApi")
-    private void downloadlongImage(final String msgId) {
+    private void downloadlongImage(final String msgId, final int code) {
         Log.d("CMCC", "download with messageId: " + msgId);
         File temp = new File(localFilePath);
         if (temp != null && temp.exists()) {
             //存在就不要下载
-            Log.d("CMCC", "存在就不要下载");
+            switch (code) {
+                case 0:
+                    PreViewUtil.preview(context, localFilePath);
+                    break;
+                case 1:
+                    //跳转到选择联系人界面只能单选
+                    Intent intent = new Intent(context, CreateGroupChatActivity.class);
+                    intent.putExtra("imgUrl", localFilePath);
+                    context.startActivity(intent);
+                    break;
+                case 2:
+                    service.submit(new Runnable() {
+                        @Override
+                        public void run() {
+                            EMImageMessageBody body = (EMImageMessageBody) message.getBody();
+                            // 其次把文件插入到系统图库
+                            try {
+                                String url = MediaStore.Images.Media.insertImage(context.getContentResolver(),
+                                        localFilePath, body.getFileName(), null);
+                                // 最后通知图库更新
+                                context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse(url)));
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                    ToastUtils.showToast(context, "保存成功了...");
+                    break;
+            }
             return;
         }
+        DialogUtils.showProgressDialog(context, "下载中");
+
         final String tempPath = temp.getParent() + "/temp_" + temp.getName();
         final EMCallBack callback = new EMCallBack() {
             public void onSuccess() {
                 Log.d("CMCC", "onSuccess");
+                DialogUtils.closeProgressDialog();
                 //预览
-                ToastUtils.showToast(context, "下载完成!");
+                switch (code) {
+                    case 0:
+                        PreViewUtil.preview(context, localFilePath);
+                        break;
+                    case 1:
+                        //跳转到选择联系人界面只能单选
+                        Intent intent = new Intent(context, CreateGroupChatActivity.class);
+                        intent.putExtra("imgUrl", localFilePath);
+                        context.startActivity(intent);
+                        break;
+                    case 2:
+                        service.submit(new Runnable() {
+                            @Override
+                            public void run() {
+                                EMImageMessageBody body = (EMImageMessageBody) message.getBody();
+                                // 其次把文件插入到系统图库
+                                try {
+                                    String url = MediaStore.Images.Media.insertImage(context.getContentResolver(),
+                                            localFilePath, body.getFileName(), null);
+                                    // 最后通知图库更新
+                                    context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse(url)));
+                                } catch (FileNotFoundException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                        ToastUtils.showToast(context, "保存成功");
+                        break;
+                }
             }
 
             public void onError(int error, String msg) {
@@ -447,6 +446,7 @@ public class EaseChatRowImage extends EaseChatRowFile {
                 if (file.exists() && file.isFile()) {
                     file.delete();
                 }
+                DialogUtils.closeProgressDialog();
             }
 
             public void onProgress(final int progress, String status) {
