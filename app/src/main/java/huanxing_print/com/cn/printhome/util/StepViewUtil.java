d26e517eaa826b87219a37e46ca77194772fa3e0
zzzsyzxl <494852668@qq.com>
2017-05-24 21:14:50 +0800

    修改部分bug

--- app/src/main/java/huanxing_print/com/cn/printhome/util/StepViewUtil.java
+++ app/src/main/java/huanxing_print/com/cn/printhome/util/StepViewUtil.java
@@ -2,13 +2,18 @@ package huanxing_print.com.cn.printhome.util;
 
 import android.content.Context;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import huanxing_print.com.cn.printhome.R;
 import huanxing_print.com.cn.printhome.view.StepLineView;
+
+import static huanxing_print.com.cn.printhome.R.id.lv;
+import static huanxing_print.com.cn.printhome.R.id.ryt;
 
 /**
  * Created by LGH on 2017/4/28.
@@ -16,9 +21,9 @@ import huanxing_print.com.cn.printhome.view.StepLineView;
 
 public class StepViewUtil {
 
-    public static void init(Context context,final View view, int step) {
+    public static void init(Context context, final View view, int step) {
         final StepLineView stepView = (StepLineView) view.findViewById(R.id.stepView);
-        TextView pickFileTv = (TextView) view.findViewById(R.id.pickFileTv);
+        final TextView pickFileTv = (TextView) view.findViewById(R.id.pickFileTv);
         TextView pickPrinterTv = (TextView) view.findViewById(R.id.pickPrinterTv);
         TextView payTv = (TextView) view.findViewById(R.id.payTv);
         if (step == StepLineView.STEP_PICK_FILE) {
@@ -33,15 +38,16 @@ public class StepViewUtil {
             pickPrinterTv.setTextColor(ContextCompat.getColor(context, R.color.stepline_red));
             payTv.setTextColor(ContextCompat.getColor(context, R.color.stepline_red));
         }
-        final LinearLayout lv = (LinearLayout) view.findViewById(R.id.lv1);
-        ViewTreeObserver vto = view.getViewTreeObserver();
-        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+        final RelativeLayout ryt = (RelativeLayout) view.findViewById(R.id.ryt);
+        ViewTreeObserver vto2 = ryt.getViewTreeObserver();
+        vto2.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                view.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                int width = lv.getWidth();
-                int padding = lv.getPaddingLeft() + lv.getPaddingRight();
-                stepView.setPadding((width - padding) / 6, 0, (width - padding) / 6, 0);
+                ryt.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                int textwidth = pickFileTv.getWidth();
+                int rytPadding = ryt.getPaddingLeft();
+                int padding =  rytPadding + textwidth / 2;
+                stepView.setPadding(padding, 0, padding, 0);
             }
         });
         stepView.setStep(step);
