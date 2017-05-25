6c88b0e77aae8eb4a67d5224ae728073467be8c8
zzzsyzxl <494852668@qq.com>
2017-05-25 22:40:08 +0800

    修改部分bug

--- app/src/main/java/huanxing_print/com/cn/printhome/wxapi/WXPayEntryActivity.java
+++ app/src/main/java/huanxing_print/com/cn/printhome/wxapi/WXPayEntryActivity.java
@@ -52,9 +52,20 @@ public class WXPayEntryActivity extends BaseActivity implements IWXAPIEventHandl
     @Override
     public void onResp(BaseResp resp) {
         if (resp.getType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
+            Logger.i(resp.errCode);
             if (SharedPreferencesUtils.getShareBoolean(WXPayEntryActivity.this, "wechatFlag", false)) {
-                Logger.i("打印支付成功");
-                EventBus.getDefault().post(new WechatPayEvent(true));
+                switch (resp.errCode) {
+                    case 0:
+                        Logger.i("打印支付成功");
+                        EventBus.getDefault().post(new WechatPayEvent(true));
+                        break;
+                    case -1:
+                        Logger.i("支付错误");
+                        break;
+                    case -2:
+                        Logger.i("用户取消");
+                        break;
+                }
             } else {
                 switch (resp.errCode) {
                     case 0:
